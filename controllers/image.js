6221b4293af602e596fbad8cf37a4749bd7b4593
Michael Aitken <mike@mikeaitken.net>
2023-02-18 18:47:10 +0800

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,52 @@
+// const dotenv = require('dotenv');
+
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'b01e7af030894279a77c8a4a7c7ae6cb',
+});
+
+
+const handleApiCall = (req, res) => {
+  // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+  // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+  // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+  // If that isn't working, then that means you will have to wait until their servers are back up. 
+
+  // Old Way:
+  // app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+
+  // New Way:
+  app.models
+    .predict(
+      {
+        id: 'face-detection',
+        name: 'face-detection',
+        version: '6dc7e46bc9124c5c8824be4822abe105',
+        type: 'visual-detector',
+      }, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+    // entries[0] --> this used to return the entries
+    // TO
+    // entries[0].entries --> this now returns the entries
+    res.json(entries[0].entries);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall
+}
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  // connect to your own database here:
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1:5432',
+    user : 'postgre',
+    password : 'T0rchW00d!',
+    database : 'face-recognition'
+  }
+});
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+
+app.get('/', (req, res)=> { res.send(db.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
+})
