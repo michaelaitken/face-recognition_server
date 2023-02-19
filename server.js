fc71a617a8f26695cde28a0f7441aed0b316d0dc
Michael Aitken <mike@mikeaitken.net>
2023-02-19 16:38:03 +0800

    Removed dotenv

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-require('dotenv').config();
+// require('dotenv').config();
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
