bef4c90c9c89d4478bf0c8f25c3b254a3ef41953
Michael Aitken <mike@mikeaitken.net>
2023-02-19 18:08:06 +0800

    Updated to gRPC API

--- controllers/image.js
+++ controllers/image.js
@@ -1,24 +1,56 @@
-const Clarifai = require('clarifai');
-// require('dotenv').config();
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
 
-const app = new Clarifai.App({
- apiKey: '1deec078bbea4d55be79eedeb4caf592',
-});
+const stub = ClarifaiStub.grpc();
 
+// This will be used by every Clarifai endpoint call
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key " + "1deec078bbea4d55be79eedeb4caf592");
 
 const handleApiCall = (req, res) => {
-  app.models
-    .predict(
+  stub.PostModelOutputs(
       {
-        id: 'face-detection',
-        name: 'face-detection',
-        version: '6dc7e46bc9124c5c8824be4822abe105',
-        type: 'visual-detector',
-      }, req.body.input)
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => res.status(400).json('unable to work with API'))
+          user_app_id: {
+              "user_id": clarifai,
+              "app_id": main
+          },
+          model_id: face-detection,
+          version_id: '6dc7e46bc9124c5c8824be4822abe105',  // This is optional. Defaults to the latest model version
+          inputs: [
+              { data: { image: { url: req.body.input } } }
+          ]
+      },
+      metadata,
+      (err, response) => {
+          if (err) {
+              throw new Error(err);
+          }
+
+          if (response.status.code !== 10000) {
+              throw new Error("Post model outputs failed, status: " + response.status.description);
+          }
+
+          // Since we have one input, one output will exist here.
+          const output = response.outputs[0];
+
+          console.log("Predicted concepts:");
+          for (const concept of output.data.concepts) {
+              console.log(concept.name + " " + concept.value);
+          }
+          res.json(response)
+      }
+  );
+  // app.models
+  //   .predict(
+  //     {
+  //       id: 'face-detection',
+  //       name: 'face-detection',
+  //       version: '6dc7e46bc9124c5c8824be4822abe105',
+  //       type: 'visual-detector',
+  //     }, req.body.input)
+  //   .then(data => {
+  //     res.json(data);
+  //   })
+  //   .catch(err => res.status(400).json('unable to work with API'))
 }
 
 const handleImage = (req, res, db) => {
